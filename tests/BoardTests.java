67e7357575df4a721b70cfb9336352130ca8219b
Irfaan Jamarussadiq <irfaanj@umd.edu>
2023-07-08 16:59:12 -0400

    Add new tests for moving pieces on board

--- tests/BoardTests.java
+++ tests/BoardTests.java
@@ -1,6 +1,8 @@
 package tests;
 
 import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 import org.junit.Test;
 import board.Board;
 import pieces.*;
@@ -33,7 +35,7 @@ public class BoardTests {
     }
 
     private void testPiecesAreOnBackRow(Board board, int rank, PieceColor color) {
-        // Test black pieces are on their back row.
+        // Test pieces are on their back row.
         assertEquals(new Rook(color), board.pieceAt(rank, 1));
         assertEquals(new Knight(color), board.pieceAt(rank, 2));
         assertEquals(new Bishop(color), board.pieceAt(rank, 3));
@@ -42,6 +44,52 @@ public class BoardTests {
         assertEquals(new Bishop(color), board.pieceAt(rank, 6));
         assertEquals(new Knight(color), board.pieceAt(rank, 7));
         assertEquals(new Rook(color), board.pieceAt(rank, 8));        
+    }
+
+    @Test
+    public void testMovePawnToD4() {
+        Board board = new Board();
+        board.movePiece(2, 4, 4, 4);
+        assertEquals(new Empty(), board.pieceAt(2, 4));
+        assertEquals(new Pawn(WHITE), board.pieceAt(4, 4));
+    }
+
+    @Test
+    public void testMoveKnightToF3() {
+        Board board = new Board();
+        board.movePiece(1, 7, 3, 6);
+        assertEquals(new Empty(), board.pieceAt(1, 7));
+        assertEquals(new Knight(WHITE), board.pieceAt(3, 6));
+    }
+
+    @Test
+    public void testMoveBishopToC4() {
+        Board board = new Board();        
+        assertThrows(IllegalArgumentException.class, () -> board.movePiece(1, 6, 4, 3));
+    }
+
+    @Test
+    public void testMoveBishopToC4AfterE4() {
+        Board board = new Board();
+        board.movePiece(2, 5, 4, 5);        
+        board.movePiece(7, 5, 5, 5);
+        assertDoesNotThrow(() -> board.movePiece(1, 6, 4, 3));
+    }
+    
+    @Test
+    public void testMoveRookToA3AfterA4() {
+        Board board = new Board();
+        board.movePiece(2, 1, 4, 1);
+        board.movePiece(7, 5, 5, 5);
+        assertDoesNotThrow(() -> board.movePiece(1, 1, 3, 1));
+    }
+
+    @Test
+    public void testBongcloudOpening() {
+        Board board = new Board();
+        board.movePiece(2, 5, 4, 5);        
+        board.movePiece(7, 5, 5, 5);
+        assertDoesNotThrow(() -> board.movePiece(1, 5, 2, 5));
     }
 
 }
