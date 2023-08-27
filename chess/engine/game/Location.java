7dd83511551b06dc1b6a16928cfc1a48bc8f2d4d
Irfaan Jamarussadiq <irfaanj@umd.edu>
2023-08-27 15:54:14 -0400

    Remove old files

--- chess/engine/game/Location.java
+++
@@ -1,7 +0,0 @@
-package chess.engine.game;
-
-public record Location(int rank, int file) {
-    public boolean isWithinBoard(int boardSize) {
-        return rank >= 1 && rank <= boardSize && file >= 1 && file <= boardSize;
-    }
-}
